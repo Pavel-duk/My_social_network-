3b08c902fbe0a1dcbd6f2a7ad9fcc0a8b1369889
Павел Кужалевич <www.duke3d@mail.ru>
2022-02-16 18:03:33 +0300

    create

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src= 'https://cdn.pixabay.com/photo/2016/08/15/18/18/bmw-1596080_960_720.png'alt = 'logo'></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src = 'https://a.d-cd.net/5e441eds-1920.jpg' alt='picture1'></img>
+            </div>
+            <div>
+                ava
+            </div>
+                <div>
+                     my posts
+                    <div>
+                        new posts
+                    </div>
+                    <div>
+                        <div>
+                            post1
+                        </div>
+                        <div>
+                            post2
+                        </div>
+                    </div>
+                </div>
+        </div>
+        
+    );
+}
+
+export default Profile;
