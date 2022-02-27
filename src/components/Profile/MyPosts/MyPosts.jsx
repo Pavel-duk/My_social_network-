f7e53e439c8f2ff804ec67647e5e3b6ce655adc9
Pavel-duk <www.duke3d@mail.ru>
2022-02-27 11:26:36 +0300

    add props, button 'i like',likes, comment

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,15 @@ import c from "./MyPosts.module.css";
 import NewPost from "./NewPost/NewPost";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
     return (
         <div className = {c.content}>
             <div className = {c.title}>
                 My posts
             </div>
             <div>
-                <Post/>
+                <Post name = "Petro" massage = "I don`t now" like = "15"/>
+                <Post name = "Wasya" massage = "I am Wasya, mmne zbs!" like = "35"/>
             </div>
             <div>
                 <NewPost/>
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import c from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div>
             <div className = {c.posts}>
                 <div className = {c.post}>
                     <img src="https://avatars.mds.yandex.net/i?id=4050c0ffbb107bdcf2de113fab36ed0f-5686193-images-thumbs&n=13" alt="avatar" />
+                    <div>
+                        {props.name} 
+                    </div>
                     <div className = {c.text}>
-                       <p>post</p> 
+                       <p>{props.massage}</p> 
+                       <div>
+                            {props.like} likes
+                            <button>I like!</button>
+                       </div>
+                       
                     </div> 
                 </div>
             </div>
